8d8e6024eb958aeec62292bf149334443e4e3cb9
AdrianSilvadoNascimento <adrianfsf10@gmail.com>
2025-04-21 19:31:41 -0300

    fix create guest flow

--- src/repositories/guest-repositories.ts
+++ src/repositories/guest-repositories.ts
@@ -2,9 +2,9 @@ import { GuestModel } from "../dtos/guest-model";
 import { GuestEntity } from "../entity/guest.entity";
 
 export abstract class GuestRepository {
-  abstract createGuest(props: GuestModel): Promise<GuestEntity>;
+  abstract createGuest(props: GuestModel): Promise<GuestEntity[]>;
   abstract getGuestById(id: string): Promise<GuestEntity>;
   abstract updateGuest(id: string, props: GuestModel): Promise<GuestEntity>;
-  abstract deleteGuest(id: string): Promise<GuestEntity>;
+  abstract deleteGuest(id: string): Promise<GuestEntity[]>;
   abstract getAllGuests(): Promise<GuestEntity[]>;
 }
